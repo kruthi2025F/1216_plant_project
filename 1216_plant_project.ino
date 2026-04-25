82d893e561f9108622acd481111c13b210244be2
saanvim-debug <saanvim@vt.edu>
2026-04-25 16:38:46 -0400

    small fixes

--- 1216_plant_project.ino
+++ 1216_plant_project.ino
@@ -1,15 +1,24 @@
-int sensor; 
+int sensor;
 const int delayTime = 1000;
-
+const int pumpPin = 7;
+const int dryThreshold = 500;
 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(pumpPin, OUTPUT);
+  digitalWrite(pumpPin, LOW);
 }
 
 void loop() {
   sensor = analogRead(A0);
   Serial.println(sensor);
-  delay(delayTime);
 
+  if (sensor < dryThreshold) {
+    digitalWrite(pumpPin, HIGH);
+    delay(2000);
+    digitalWrite(pumpPin, LOW);
+    delay(5000);
+  }
+
+  delay(delayTime);
 }
